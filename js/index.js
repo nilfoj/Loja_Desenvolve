2251e4432f4ced15c0f0d25f9f8a0e560848af67
nilfoj <nilfoj@hotmail.com>
2024-05-01 20:44:49 -0300

    Att.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Banner_01
 const texto1 = document.getElementById("app20");
 const texto2 = document.getElementById("frete");
 
@@ -15,4 +16,23 @@ function atualizarBanner() {
     mostrarTexto2 = !mostrarTexto2;
 }
 
+
+// Banner_02
+
 setInterval(atualizarBanner, 2000); /* Alterar o texto a cada 5 segundos */
+
+const imagens = document.querySelectorAll('.banner-image');
+const intervalo = 5000; // Tempo em milissegundos (5 segundos)
+let indiceAtual = 0;
+
+function mostrarImagem(indice) {
+    imagens.forEach(imagem => imagem.classList.remove('active'));
+    imagens[indice].classList.add('active');
+}
+
+mostrarImagem(indiceAtual);
+
+setInterval(() => {
+    indiceAtual = (indiceAtual + 1) % imagens.length;
+    mostrarImagem(indiceAtual);
+}, intervalo);
